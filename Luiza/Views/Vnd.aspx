6a791e2e6729e7310f887d58e1c02cad6a64b4ef
Максим Щеглов <m.cheglov@gmail.com>
2018-12-25 02:01:10 +0700

    vnd

--- Luiza/Views/Vnd.aspx
+++ Luiza/Views/Vnd.aspx
@@ -3,7 +3,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="title" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Content" Runat="Server">
-    <asp:DropDownList ID="streetDL" runat="server">
-    </asp:DropDownList>
+    <table class="w-100">
+        <tr>
+            <td>
+                <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" CssClass="form-check-label" OnTextChanged="TextBox1_TextChanged" Width="150px"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                <asp:DropDownList ID="streetDL" runat="server" Width="150px">
+                </asp:DropDownList>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
 </asp:Content>
 
--- Luiza/Views/Vnd.aspx.cs
+++ Luiza/Views/Vnd.aspx.cs
@@ -10,11 +10,31 @@ public partial class Views_Vnd : System.Web.UI.Page
     toSQLDataContext db = new toSQLDataContext();
     protected void Page_Load(object sender, EventArgs e)
     {
-        var streets = from o in db.Vnd_Nsk
-                      select o;
-        for (int i=0;i< streets.Count(); i++)
+        streetDL.Visible = false;
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        streetDL.Items.Clear();
+        if (TextBox1.Text.Length > 0)
         {
-            streetDL.Items.Add(streets.ToList().ElementAt(i).formalname.ToString() + " " + streets.ToList().ElementAt(i).shortname.ToString());
+            var streets = (from o in db.Vnd_Nsk
+                           where o.formalname.Contains(TextBox1.Text)
+                           select o).ToList();
+            for (int i = 0; i < streets.Count(); i++)
+            {
+                streetDL.Items.Add(streets[i].shortname + " " + streets[i].formalname);
+            }
         }
+        TextBox1.Text = "";
+        if (streetDL.Items.Count > 0)
+        {
+            streetDL.Visible = true;
+        }
+        else
+        {
+            streetDL.Visible = false;
+        }
+
     }
 }
